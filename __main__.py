86971f91bd6578802e75eb85fe3325df717ff682
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-06 00:52:38 +0900

    🔥🐛 fixes
    🔥 Remove: unnecessary ignored lines
    🐛 Fix: check omission in same name file checker

--- __main__.py
+++ __main__.py
@@ -1,10 +1,8 @@
 from sys import argv
 from settings import infer_on
 from diffusers import UnCLIPPipeline
-#from diffusers import UnCLIPImageVariationPipeline
 import torch
 import os
-#import PIL
 
 prompt = " ".join(argv[1:])
 
@@ -14,15 +12,14 @@ print("Inference on", infer_on.upper())
 torch_dtype = torch.float16 if infer_on == "cuda" else torch.float32
 
 generate = UnCLIPPipeline.from_pretrained("kakaobrain/karlo-v1-alpha", torch_dtype=torch_dtype).to(infer_on)
-#variation = UnCLIPImageVariationPipeline.from_pretrained("kakaobrain/karlo-v1-alpha-image-variations", torch_dtype=torch_dtype).to(infer_on)
 
 image = generate(prompt).images[0]
-#image_variabled = variation(image).images[0]
 
+prompt = prompt.replace(" ", "-")
 i = 0
 while os.path.exists(f"outputs/{prompt}_{i}.png"):
     i += 1
-filename = f"outputs/{prompt.replace(' ', '-')}_{i}.png"
+filename = f"outputs/{prompt}_{i}.png"
 image.save(filename)
 
 print(f"Done: " + filename)
